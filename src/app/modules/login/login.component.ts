8e6473c26460049ca94a724cda426f8387d3a364
lanxuexing <lanxuexing313wsr@163.com>
2020-02-16 15:36:55 +0800

    feat: login FormGroup

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) {
+    // 第二种：使用工厂方法
+    this.form = this.fb.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+    // 第一种：直接new FormControl
+    // this.form = new FormGroup({
+    //   email: new FormControl('', Validators.compose([Validators.email, Validators.required])),
+    //   password: new FormControl('', Validators.required)
+    // });
+  }
+
+  // 登录
+  onSubmit(from: FormGroup, ev: Event) {
+    console.log('登录: ', from, ev);
   }
 
 }
